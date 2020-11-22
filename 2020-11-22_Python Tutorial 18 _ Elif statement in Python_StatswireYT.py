b4096471796489f337170c59cbfdb5be25db928e
erniejie <46618858+Erniejie@users.noreply.github.com>
2020-11-22 13:03:20 +0000

    Add files via upload

---
+++ 2020-11-22_Python Tutorial 18 _ Elif statement in Python_StatswireYT.py
@@ -0,0 +1,27 @@
+
+#EXAMPLE 1
+"""
+a=23
+b=23
+if a > b:
+    print("A is Greater than B")
+elif a==b:                   #---Otherwise
+    print("A is Equal to B")
+
+else:
+    print("A is Less than B")
+"""
+
+# EXAMPLE 2:
+x=39
+if x==23:
+    print("X is 23")
+
+elif x==36:
+    print("X is 36")
+
+else:
+    print("X is not 36")
+
+
+
